5806def0b53b5896641d36d5f0606de22d21196f
Wayne Sun <wtse.sun@gmail.com>
2020-09-03 21:29:08 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import React, { useState, useRef, useCallback } from "react";
-import uuidV4 from "uuid/v4";
 
 import "./styles/App.css";
+import useBookSearch from "./useBookSearch";
+import Loading from "./Loading";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
+  const { error, hasMore, books, loading } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   return (
     <div className="screen">
       <input
         className="input"
         placeholder="Search books here"
         type="text"
-      ></input>
-      <div className="title">title</div>
-      <div className="title">title</div>
-      <div className="title">title</div>
-      <div className="title">title</div>
-      <div className="title">Loading...</div>
-      <div className="title">---Error---</div>
+        value={query}
+        onChange={(e) => handleSearch(e)}
+      />
+      <div>
+        {books.map((book, idx) => {
+          if (idx + 1 !== books.length) {
+            return (
+              <div key={book} className="title">
+                {book}
+              </div>
+            );
+          } else {
+            return (
+              <div key={book} ref={lastBookElementRef} className="title">
+                {book}
+              </div>
+            );
+          }
+        })}
+      </div>
+      {loading ? <Loading /> : null}
+      {error ? <div>ERROR</div> : null}
     </div>
   );
 }
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -2,15 +2,42 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useBookSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
   useEffect(() => {
+    setBooks([]);
+  }, [query]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel = null;
+
     axios({
       method: "GET",
       url: "http://openlibrary.org/search.json",
       params: { q: query, page: pageNumber },
-    }).then((res) => {
-      console.log(res.data);
-    });
-  }, [pageNumber]);
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setBooks((prevBook) => {
+          return [
+            ...new Set([...prevBook, ...res.data.docs.map((b) => b.title)]),
+          ];
+        });
+        setHasMore(res.data.docs.length > 0);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
 
-  return;
+    return () => cancel();
+  }, [pageNumber, query]);
+
+  return { loading, error, books, hasMore };
 }
